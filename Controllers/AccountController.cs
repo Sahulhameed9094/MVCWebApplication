dff5181c055fffa893bd7eaac57ce4cc55fa1bb7
Sahul Hameed <sahulhameed9094@gmail.com>
2025-09-25 19:31:56 +0530

    added login page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCWebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
